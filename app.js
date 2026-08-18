5ed4b5702a8cb3f1bd05d4e358b4ca9aced1050d
Utkarsh Uday Singh <utkarshudaysingh@Utkarshs-MacBook-Air.local>
2026-08-18 16:37:50 +0530

    Add Docker configuration and Cloud Run support

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'logoswayatt.png'));
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
