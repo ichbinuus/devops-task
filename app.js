9fe542ac8c474328507bcdd0c45f132f4cdf0833
Utkarsh Uday Singh <utkarshudaysingh@Utkarshs-MacBook-Air.local>
2026-08-21 13:09:41 +0530

    Test automatic CI/CD pipeline

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on ports ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
